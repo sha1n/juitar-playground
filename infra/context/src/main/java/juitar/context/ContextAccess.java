2813e689f5fd065f922f738a95a87108714ebd98
sha1n <shai.nagar@gmail.com>
2013-01-25 11:20:04 +0200

    Added access to spring app context from the main context API

--- infra/context/src/main/java/juitar/context/ContextAccess.java
+++ infra/context/src/main/java/juitar/context/ContextAccess.java
@@ -1,10 +1,25 @@
 package juitar.context;
 
+import org.springframework.context.ApplicationContext;
+
 /**
  * @author sha1n
  * Date: 1/4/13
  */
 public class ContextAccess {
+
+    private static ApplicationContext applicationContext = null;
+
+    public static void setApplicationContext(ApplicationContext applicationContext) {
+        if (ContextAccess.applicationContext != null) {
+            throw new UnsupportedOperationException("Context already set.");
+        }
+        ContextAccess.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 
     private static final InheritableThreadLocal<Context> localContext = new InheritableThreadLocal<Context>() {
         @Override
