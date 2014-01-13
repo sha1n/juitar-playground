2bcfc426dc4809e9511412c640515714ee3b50a5
Shai <shai.nagar@gmail.com>
2014-01-13 21:53:56 +0200

    Removed feature-flags module

--- netty/web/rest/src/main/java/juitar/web/rest/resource/Status.java
+++ netty/web/rest/src/main/java/juitar/web/rest/resource/Status.java
@@ -1,8 +1,8 @@
 package juitar.web.rest.resource;
 
-import org.juitar.flags.*;
-
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,44 +21,4 @@ public class Status {
                 "be broken into many many many chunks, so I can debug my stupid client...").build();
     }
 
-    @GET
-    @Path("/feature")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response feature(@QueryParam("flag") String flagName,
-                            @QueryParam("scope")
-                            @DefaultValue("null")
-                            String scopeName) {
-        Flag flag = Flags.get(flagName);
-        Scope scope = Scopes.get(scopeName);
-        MyFeature instance = new FeatureFactory(flag).getInstance("", scope);
-        return Response.ok().entity(instance.getValue()).build();
-    }
-
-
-    private static class FeatureFactory extends AbstractFlaggedFeatureFactory<MyFeature, String> {
-
-        protected FeatureFactory(Flag flag) {
-            super(flag);
-        }
-
-        @Override
-        protected MyFeature getFeatureFlagOn(String arg) {
-            return new MyFeature() {
-                @Override
-                public String getValue() {
-                    return "Flag is on!";
-                }
-            };
-        }
-
-        @Override
-        protected MyFeature getFeatureFlagOff(String arg) {
-            return new MyFeature() {
-                @Override
-                public String getValue() {
-                    return "Flag is off!";
-                }
-            };
-        }
-    }
 }
